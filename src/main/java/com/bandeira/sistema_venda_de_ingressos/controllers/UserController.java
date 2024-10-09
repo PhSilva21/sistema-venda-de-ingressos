41fe88f0e8bfacf884c86df9af0b3477b1851bfb
Pedro Bandeira <pedro.amp003@gmail.com>
2024-10-09 16:19:46 -0300

    Add UserController

---
+++ src/main/java/com/bandeira/sistema_venda_de_ingressos/controllers/UserController.java
@@ -0,0 +1,60 @@
+package com.bandeira.sistema_venda_de_ingressos.controllers;
+
+import com.bandeira.sistema_venda_de_ingressos.dtos.CreateUserDTO;
+import com.bandeira.sistema_venda_de_ingressos.dtos.UpdateEmailDTO;
+import com.bandeira.sistema_venda_de_ingressos.dtos.UpdatePasswordDTO;
+import com.bandeira.sistema_venda_de_ingressos.dtos.UpdateUserDTO;
+import com.bandeira.sistema_venda_de_ingressos.models.User;
+import com.bandeira.sistema_venda_de_ingressos.services.UserService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PostMapping("/create-user")
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserDTO request) throws MessagingException, UnsupportedEncodingException {
+        userService.createUser(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDetails> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PutMapping("/update-user")
+    public ResponseEntity<Void> updateUser(@RequestBody UpdateUserDTO updateUserDTO) {
+        userService.updateUser(updateUserDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/update-email")
+    public ResponseEntity<Void> updateEmail(@RequestBody UpdateEmailDTO request) throws MessagingException, UnsupportedEncodingException {
+        userService.updateEmail(request);
+        return  ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/update-password")
+    public ResponseEntity<User> updatePassword(@RequestBody UpdatePasswordDTO request) {
+        userService.updatePassword(request);
+        return ResponseEntity.ok().build();
+    }
+}
